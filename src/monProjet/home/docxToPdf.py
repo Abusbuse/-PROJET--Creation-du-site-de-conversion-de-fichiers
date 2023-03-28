590340852740dd1203e6b5b5bb48be547d2a82fc
Wittmann Gregory <gregory.wittmann4@etu.univ-lorraine.fr>
2023-03-28 09:30:19 +0200

    Mise au premier plan de la fenetre de nav file

--- src/monProjet/home/docxToPdf.py
+++ src/monProjet/home/docxToPdf.py
@@ -2,23 +2,34 @@ from django.http import HttpResponse
 import os
 from tkinter import  filedialog
 from docx2pdf import convert
+from tkinter import *
 
 #fonction qui prend le fichier docx et le convertit en pdf
 def convertFileDocxToPdf(request):
-    filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("Docx files" ,"*.docx"),("all files","*.*")))
+    root=Tk()
+    root.withdraw()
+    root.lift()
+    root.attributes('-topmost',True)
+    #Ouvre une fenêtre pour sélectionner le fichier
+    filename = filedialog.askopenfilename(parent=root, initialdir = "/",title = "Select file",filetypes = (("Docx files" ,"*.docx"),("all files","*.*")))
+    root.destroy()
     
-    #Récupère la taille du fichier
-    sizeFile= os.path.getsize(filename)
-    
-    #si l'utilisateur annule l'opération
+    #si l'utilisateur ne sélectionne pas de fichier
     if filename == "":
         print("Aucun fichier sélectionné")
-    elif sizeFile > 10000000:
-        print("Fichier trop volumineux")
+        alert ="('Aucun fichier sélectionné');"
     else:
-        #télécharge le fichier converti dans le dossier de téléchargement du pc de l'utilisateur
-        convert(filename, os.path.expanduser("~/Downloads/"))
-        
-        print("Fichier converti avec succès !")
+        #Récupère la taille du fichier
+        sizeFile= os.path.getsize(filename)
+        if sizeFile > 10000000:
+            print("Fichier trop volumineux")
+            alert ="('Fichier trop volumineux');"
+            
+        else:
+            #télécharge le fichier converti dans le dossier de téléchargement du pc de l'utilisateur
+            convert(filename, os.path.expanduser("~/Downloads/"))
+            
+            print("Fichier converti avec succès !")
+            alert ="('Fichier converti');"
 
-    return HttpResponse("""<html> <script> alert("Fichier converti");window.location.replace('/convert/');</script> </html>""")
+    return HttpResponse("""<html> <script> alert"""+ alert + """window.location.replace('/convert/');</script> </html>""")
